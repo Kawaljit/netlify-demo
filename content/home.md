133b978be38d3b839f346e6fe6a09d308afb6682
kjsingh <in.kawal@gmail.com>
2022-05-26 22:45:21 +1000

    Add files

---
+++ content/home.md
@@ -0,0 +1,15 @@
+---
+title: List Items
+date: 2019-03-17T19:31:20.591Z
+lists:
+  - description: orem ipsum dolor sit amet, consectetur adipiscing elit.
+    name: List 1
+  - description: Praesent tortor elit, dictum et purus in, fermentum finibus orci.
+    name: List 2
+  - description: Integer quis diam in leo vestibulum vehicula. Aenean pharetra, turpis vitae faucibus feugiat, elit lectus congue velit, vitae facilisis libero tortor ut magna.
+    name: Link 3
+---
+
+Welcome to my awesome page about cats of the internet.
+
+This page is built with NextJS, and content is managed in Netlify CMS
---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import { attributes, react as HomeContent } from '../content/home.md';
+
+function Home() {
+  let { title, lists } = attributes;
+  return (
+    <>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <article>
+        <h1>{title}</h1>
+        <HomeContent />
+        <ul>
+          {lists.map((cat, k) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </>
+  );
+}
+
+export default Home;
